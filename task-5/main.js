2586a0a7ebe8b684cfab86183ce15c08e0c4d724
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-20 23:15:40 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -6,4 +6,17 @@ prompt.get('input', function (err, result) {
   
     // Write code here
   
+  
+        let num = 5;
+        let arr = [];
+        for(let i = 0;i<num;i++){
+            arr.push(new Array(num).fill('*'));
+        }   
+        console.log(arr);
+
+
+        // for(let i = 0;i<arr.length;i++){
+        //     console.log(arr[i].join(''))
+        // }
+  
 });
