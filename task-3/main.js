f8cd53b38cb93b2bbd85bb654822e16dbb81d99e
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-20 22:57:02 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -8,3 +8,30 @@ let matrix = [
 ]
 
 // Write code here. 
+
+
+let matrix = [
+    [2, 5, 3, 1, 4],
+    [3, 5, 2, 3, 4],
+    [4, 6, 1, 2, 3],
+    [4, 5, 6, 1, 7]
+]
+
+
+
+let min = matrix[0][0];
+for(let i = 0;i<matrix.length;i++){
+    for(let j = 0;j<matrix[i].length;j++){
+        // console.log(`${matrix[i][j]}`);
+        if(matrix[i][j]<min){
+            min = matrix[i][j];
+        }
+    }
+}
+console.log(min);
+for(let i = 0;i<matrix.length;i++){
+    // console.log(matrix[i])
+    if(matrix[i].includes(min)){
+        console.log(i+1);
+    }
+}
